fc62e49ea98bdb3987c5a899eaac78c03beb103a
Phil <224063001@qq.com>
2018-01-24 12:37:43 +0800

    good

--- test.js
+++ test.js
@@ -1,14 +1,16 @@
 var compiler = require('./src/index.js');
 var string1=
 `For  group_node_abc:
-  in initial :  proceed to signing on transaction of 100 to feth1026f01634a
-  in signing:
+  in initial :  proceed to <signing> on transaction of 100 to feth1026f01634a
+  in <signing>:
     proceed to activate on accepting license license_A`;
 
 var string2 =
-`for aaa-1-1, group_NODE_assv, group_user_assbv :
+`for group_user_bbb:
   in initial :
-    terminate`;
+    proceed to signing on transaction of 200 to feth209fa4da1a4
+  in signing:
+    proceed to <activate> on license license_A`;
 
    console.log('start gen');
   //  var re = compiler.compile(string1, 'beautify');
@@ -16,7 +18,7 @@ var string2 =
   //  var str = re.stringArray.join(' ').replace(/\n\s/g,'\n');
   //  console.log(str);
    var re2 = compiler.compile(string2);
-   // console.log(re2);
+   console.log(re2);
    // console.log(re2.policy_segments[0].users);
    // console.log(re2.policy_segments[0].state_transition_table);
   //  console.log(JSON.stringify(re2.policy_segments[0].state_transition_table));
