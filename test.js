2ede50e02b9b5e826c339cbe1e588a5e31cdef5a
Phil <224063001@qq.com>
2018-02-02 11:15:27 +0800

    good

--- test.js
+++ test.js
@@ -1,24 +1,30 @@
 var compiler = require('./src/index.js');
 var string1=
-`For  group_node_abc:
-  in initial :  proceed to <signing> on transaction of 100 to feth1026f01634a
+`For  group_user_abcd:
+  in initial :  proceed to <signing> on every cycle
   in <signing>:
-    proceed to activate on accepting license license_A`;
+    proceed to activate on accepting license feth233dbc32069`;
 
 var string2 =
 `for public:
 in initial:
 proceed to <good> on accepting license e759419923ea25bf6dff2694391a1e65c21739ce`
 
+var str3 =
+`for public :
+  in initial :
+    proceed to <signing> on transaction of 100 to feth233dbc32069
+  in <signing> :
+    proceed to activate on accepting license e759419923ea25bf6dff2694391a1e65c21739ce`
    console.log('start gen');
   //  var re = compiler.compile(string1, 'beautify');
    // console.log(re);
   //  var str = re.stringArray.join(' ').replace(/\n\s/g,'\n');
   //  console.log(str);
-   var re2 = compiler.compile(string2);
-   console.log(re2);
-   console.log(re2.policy_segments[0].users);
+   var re2 = compiler.compile(string1);
+   // console.log(re2);
+   // console.log(re2.policy_segments[0].users);
    // console.log(re2.policy_segments[0].state_transition_table);
-  //  console.log(JSON.stringify(re2.policy_segments[0].state_transition_table));
+   console.log(JSON.stringify(re2.policy_segments[0].state_transition_table));
 
-    console.log(compiler.compile(string2, 'beautify').stringArray.splice(1).join(' ').replace(/\n\s/g,'\n'));
+    // console.log(compiler.compile(str3, 'beautify').stringArray.splice(1).join(' ').replace(/\n\s/g,'\n'));
